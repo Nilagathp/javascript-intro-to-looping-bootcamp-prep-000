23d665208270238b48d3da6b22d15920e3a92ca3
empathetic-parameter-3158 <nilagathp@gmail.com>
2019-05-20 13:48:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,24 +1,32 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+  for (var i = 0; i < 25; i++) {
+    if (i <= 1) {
+      array.push (`I am ${i} strange loop.`);
+    } else {
+      array.push (`I am ${i} strange loops.`);
+    }
   }
-  return array
+  return array;
 }
 
-function whileLoop(number) {
-  while (number != 0) {
-    console.log(number)
-    number--
+function whileLoop(n) {
+  while (n > 0) {
+    n -= 1;
+    console.log(n);
   }
   return 'done'
 }
-let i = 0;
+
+var i = 0;
+
 function incrementVariable() {
   i = i + 1;
 }
+
 function doWhileLoop(array) {
   do {
     array.pop();
-  } while (array.length > 0 && incrementVariable());
-return array
+  } while (array.length > 0);
+  return array;
 }
+
